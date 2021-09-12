761a7678d1343a93eecbfccd406c4d857ebe98ef
Ariel Gianatiempo <gianatiempo@gmail.com>
2021-09-12 20:17:08 -0300

    added eslint/prettier config

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { Layout } from "antd";
+import { Layout } from 'antd';
 
 const { Header, Content } = Layout;
 
-const headerStyle = { position: "fixed", zIndex: 1, width: "100%" };
-const contentStyle = {
-  padding: "24px 50px 0",
-  marginTop: 64,
-  height: "calc(100vh - 64px)",
-};
+const headerStyle = { position: 'fixed', zIndex: 1, width: '100%' };
+const contentStyle = { padding: '24px 50px 0', marginTop: 64, height: 'calc(100vh - 64px)' };
 
 const App = () => (
   <Layout>
     <Header style={headerStyle}>
-      <div className="logo" />
+      <div className='logo' />
     </Header>
     <Content style={contentStyle}>Content</Content>
   </Layout>
