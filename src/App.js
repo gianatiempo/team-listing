fb0cff4eed58784fc14cd7d9dcf8db023a61a082
Ariel Gianatiempo <gianatiempo@gmail.com>
2021-09-12 20:39:03 -0300

    add logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Layout } from 'antd';
+import logo from './assets/logo.svg';
 
 const { Header, Content } = Layout;
 
 const headerStyle = { position: 'fixed', zIndex: 1, width: '100%' };
+const logoStyle = { marginTop: '-15px' };
 const contentStyle = { padding: '24px 50px 0', marginTop: 64, height: 'calc(100vh - 64px)' };
 
 const App = () => (
   <Layout>
     <Header style={headerStyle}>
-      <div className='logo' />
+      <img src={logo} alt='App Logo' style={logoStyle} />
     </Header>
     <Content style={contentStyle}>Content</Content>
   </Layout>
