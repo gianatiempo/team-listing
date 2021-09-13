47fe40107ad7476af58f45f4c545da777c51577b
Ariel Gianatiempo <gianatiempo@gmail.com>
2021-09-13 00:27:09 -0300

    initial team page

--- src/mocks/teamHandler.js
+++ src/mocks/teamHandler.js
@@ -1,6 +1,18 @@
 import { rest } from 'msw';
 
 const random = max => Math.floor(Math.random() * max) + 1;
+const skills = [`Java`, `React`, `Redis`, 'MongoDb', 'AWS', 'Python', 'SQL', '.NET'];
+const getRandomSkills = num =>
+  skills
+    .slice(0)
+    .sort(() => 0.5 - Math.random())
+    .splice(0, num);
+
+const getRandomRole = () =>
+  ['Developer', 'Functional Analyst', 'Tester', 'Project Manager', 'UX Expert', 'Product Owner']
+    .slice(0)
+    .sort(() => 0.5 - Math.random())
+    .splice(0, 1);
 
 const handlers = [
   rest.get('/api/team', (req, res, ctx) => {
@@ -17,10 +29,37 @@ const handlers = [
         lvl4: `Mgmt 4 #${random(4)}`, // up to 4 lvl 4
         lvl5: `Mgmt 5 #${random(3)}`, // up to 3 lvl 5
         lvl6: `Mgmt 6 #${random(2)}`, // up to 2 lvl 6
-        skills: [`Java`, `React`, `Redis`] // up to 4 team skills
+        skills: getRandomSkills(3) // up to 3 team skills
       });
     }
     return res(ctx.json({ data }));
+  }),
+
+  rest.get('/api/team/:id', (req, res, ctx) => {
+    const { id } = req.params;
+    const members = random(14);
+    return res(
+      ctx.json({
+        id: id,
+        name: `Team Number ${id}`,
+        objective: 'This team was created to push forward some project.',
+        members, // up to 14 team members
+        building: `Main Tech ${random(4)}`, // up to 4 buildings
+        lvl1: `Mgmt 1 #${random(3)}`, // up to 3 lvl 1
+        lvl2: `Mgmt 2 #${random(4)}`, // up to 4 lvl 2
+        lvl3: `Mgmt 3 #${random(5)}`, // up to 5 lvl 3
+        lvl4: `Mgmt 4 #${random(4)}`, // up to 4 lvl 4
+        lvl5: `Mgmt 5 #${random(3)}`, // up to 3 lvl 5
+        lvl6: `Mgmt 6 #${random(2)}`, // up to 2 lvl 6
+        skills: getRandomSkills(3), // up to 3 team skills
+        teamMembers: [...Array(members)].map((item, i) => ({
+          name: `Member ${i + 1} Name`,
+          bio: `Bio of Member ${i + 1} in the company`,
+          skills: getRandomSkills(5),
+          role: getRandomRole()
+        }))
+      })
+    );
   })
 ];
 
--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,25 +1,54 @@
-import { Row, Col, Card } from 'antd';
-import { Link } from 'react-router-dom';
+import { Row, Col, Card, Typography, Tag, Space, Divider } from 'antd';
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
-const Team = () => (
-  <Row gutter={16}>
-    <Col span={6}>
-      <Card>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-    </Col>
-    <Col span={18}>
-      <Card>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>
-          And you can <Link to='/'>Go Back</Link>
-        </p>
-      </Card>
-    </Col>
-  </Row>
-);
+const { Title, Text } = Typography;
+
+const Team = () => {
+  const { id } = useParams();
+  const [team, setTeam] = useState({});
+
+  useEffect(() => {
+    fetch(`/api/team/${id}`)
+      .then(resp => resp.json())
+      .then(setTeam);
+  }, [id]);
+
+  return (
+    <Row gutter={16}>
+      <Col span={6}>
+        <Card>
+          <Title level={2}>{team.name}</Title>
+          <Space direction='vertical' split={<Divider />}>
+            <Text>{team.objective}</Text>
+            <Text>Members: {team.members}</Text>
+            <Text>
+              Skills:{' '}
+              {team.skills?.map(s => (
+                <Tag key={s}>{s}</Tag>
+              ))}
+            </Text>
+            <Text>Building:{team.building}</Text>
+            <Space direction='horizontal' split={<Divider type='vertical' />}>
+              {team.lvl1}
+              {team.lvl2}
+              {team.lvl3}
+              {team.lvl4}
+              {team.lvl5}
+              {team.lvl6}
+            </Space>
+            <Link to='/'>Go Back to List</Link>
+          </Space>
+        </Card>
+      </Col>
+      <Col span={17}>
+        <Card>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 export default Team;
